9c546047c1e93bc9aac19f0a0632d2b65c122a48
Mansor Almossa <malmossa@gmail.com>
2023-08-10 09:10:28 -0700

    completed

---
+++ TacoCat_MVC_CH/TacoCat_MVC_CH/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TacoCat_MVC_CH.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
